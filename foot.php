81084bff1ea75512cf09b08e79c4042c22ad12c3
marwanto606 <marwanto606@gmail.com>
2017-07-01 11:03:10 +0700

    Create foot.php

---
+++ foot.php
@@ -0,0 +1,8 @@
+<div class="list5"><a href="#top">^Top</a> - <a href="/disclaimer.php">Disclaimer</a></div></div>
+
+<div class="head" align="center">
+<span>
+
+
+<font size="1">© 2016 Power By <a href="http://maxmp3.pw" rel="dofollow" title="Maxmp3.pw"><b>Maxmp3.pw</b></a></font></div>
+</body></html>
